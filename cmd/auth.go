a9c79ae77de6482815377e616da62e3fbf0c97c4
shellfly <shell0fly@gmail.com>
2023-04-09 14:02:04 +0800

    feat: auth command

--- cmd/auth.go
+++ cmd/auth.go
@@ -9,12 +9,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const AuthHint = `Open the URL in a web browser to link your install ID with your account:
+
+https://ai-talk.app/connect?install_id=%s
+
+
+This will associate all talks uploaded from this machine (past and future ones) to your account, and allow you to manage them at ai-talk.app.
+`
+
 // authCmd represents the auth command
 var authCmd = &cobra.Command{
 	Use:   "auth",
 	Short: "authenticate and connect talks",
 	Long:  `connect talks generated on this machine to an ai-talk.app account.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("auth called")
+		hint := fmt.Sprintf(AuthHint, getInstallID())
+		fmt.Println(hint)
 	},
 }
